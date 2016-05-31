52fcd8447ca8cf4ccf3a774e20ad3b7338bfe2da
Romain Bruckert <romain.bruckert.esc@gmail.com>
2016-05-31 12:15:27 +0200

    Added doctrine extensions (RAND, IFNULL, DATE_FORMAT)

---
+++ Extension/DateFormat.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Medical\DoctrineDQLBundle\Extension;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class DateFormat extends FunctionNode
+{
+    public $dateExpression = null;
+    public $patternExpression = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->dateExpression = $parser->ArithmeticExpression();
+        $parser->match(Lexer::T_COMMA);
+        $this->patternExpression = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'DATE_FORMAT(' .
+            $this->dateExpression->dispatch($sqlWalker) . ', ' .
+            $this->patternExpression->dispatch($sqlWalker) .
+        ')';
+    }
+}
